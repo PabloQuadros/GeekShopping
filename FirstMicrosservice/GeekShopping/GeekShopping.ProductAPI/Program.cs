291c1ac1256c08f47a6ee74ac6513e6b042999d6
Pablo Quadros <pablo.aquadros@gmail.com>
2023-01-09 16:19:51 -0300

    Finalizando Secao 8

--- FirstMicrosservice/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ FirstMicrosservice/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -7,7 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 string mySqlConnection = builder.Configuration.GetConnectionString("MySQLConnectionString");
 builder.Services.AddDbContextPool<MySQLContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
